e8121d7312730775db39ac76f3715782246c7605
Lucian Moldovan <lucianmldvn@outlook.com>
2016-11-17 09:19:19 +0200

    - spelling

--- generators/component/templates/test.js
+++ generators/component/templates/test.js
@@ -6,7 +6,7 @@ var expect = chai.expect;
 var <%= componentName %> = require('../../components/<%= componentFileName %>');
 
 describe('Component <%= componentName %>', function <%= componentName %>Test() {
-    it('should exists', function (done) {
+    it('should exist', function (done) {
         expect(<%= componentName %>).not.to.be.an('undefined');
         expect(<%= componentName %>).to.be.an('object');
         done();
--- generators/lib/templates/test.js
+++ generators/lib/templates/test.js
@@ -6,7 +6,7 @@ var expect = chai.expect;
 var <%= libName %> = require('../../lib/<%= libFileName %>');
 
 describe('Lib <%= libName %>', function <%= libName %>Test() {
-    it('should exists', function (done) {
+    it('should exist', function (done) {
         expect(<%= libName %>).not.to.be.an('undefined');
         expect(<%= libName %>).to.be.an('object');
         done();
